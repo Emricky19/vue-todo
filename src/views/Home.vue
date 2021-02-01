f958637c48bd9552e3c26dc75cafda614ccec93a
Emrick19 <emricky17@gmail.com>
2021-02-01 11:51:54 +0100

    Get todos from store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,47 +6,58 @@
 </template>
 
 <script>
+import AddTodo from "../components/AddTodo";
+import Todos from "../components/Todos";
 
-import AddTodo from "../components/AddTodo"
-import Todos from "../components/Todos"
-
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Todos,
-    AddTodo
+    AddTodo,
   },
   data: () => {
     return {
-      todos: []
-    }
+      
+    };
   },
   methods: {
     deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => {
-        console.log(res)
-        this.todos = this.todos.filter(todo => todo.id !== id)
-      })
-      .catch(err => console.log(err))
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then((res) => {
+          console.log(res);
+          this.todos = this.todos.filter((todo) => todo.id !== id);
+        })
+        .catch((err) => console.log(err));
     },
-    addTodo(newTodo){
-      const { title, completed } = newTodo
-      axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed})
-      .then(response => this.todos = [...this.todos, response.data])
-      .catch(err => console.log(err))
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((response) => (this.todos = [...this.todos, response.data]))
+        .catch((err) => console.log(err));
+    },
+  },
+  computed: {
+    todos() {
+      return this.$store.getters.allTodos
     }
   },
-  created(){
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-    .then(response => this.todos = response.data)
-    .catch(err => console.log(err))
-  }
-}
+  mounted() {
+    this.$store.dispatch("getTodos");
+  },
+  created() {
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+    //   .then((response) => (this.todos = response.data))
+    //   .catch((err) => console.log(err));
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
