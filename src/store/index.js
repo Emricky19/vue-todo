d6fa3dbebdad69bf23406a0e9cd34eb9b88e140d
Emrick19 <emricky17@gmail.com>
2021-02-01 12:46:21 +0100

    Implement deleteTodo

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,7 @@ const state = {
 };
 
 const getters = {
-    allTodos: (state) => state.todos
+  allTodos: (state) => state.todos,
 };
 
 const actions = {
@@ -17,17 +17,42 @@ const actions = {
     axios
       .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
       .then((response) => {
-        console.log(response.data);
         commit("SET_POSTS", response.data);
       })
       .catch((error) => console.log(error));
   },
+  deleteTodo: ({ commit }, id) => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((res) => {
+          console.log(res);
+        commit("DELETE_TODO", id);
+      })
+      .catch((err) => console.log(err));
+  },
+  addTodo: ({commit}, newTodo) => {
+    const { title, completed } = newTodo;
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title,
+        completed,
+      })
+      .then((response) => commit('ADD_TODO', response.data))
+      .catch((err) => console.log(err));
+  }
 };
 
 const mutations = {
   SET_POSTS: (state, todos) => {
     state.todos = todos;
   },
+  DELETE_TODO: (state, id) => {
+      state.todos = state.todos.filter(todo => todo.id !== id)
+      console.log(state.todos)
+  },
+  ADD_TODO: (state, todo) => {
+      state.todos = [...state.todos, todo]
+  }
 };
 
 export default new Vuex.Store({
